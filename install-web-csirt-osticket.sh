f9a8b8493dd3cc9c6988d1538614643a7fba4a0e
razorzodiac <155718912+razorzodiac@users.noreply.github.com>
2024-01-05 16:22:51 +0800

    Add files via upload

---
+++ install-web-csirt-osticket.sh
@@ -0,0 +1,15 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html;charset=ISO-8859-1"/>
+<title>Error 404 no such item</title>
+</head>
+<body><h2>HTTP ERROR 404 no such item</h2>
+<table>
+<tr><th>URI:</th><td>/service/home/rizal.damar@bssn.go.id/Briefcase/install-web-csirt-osticket.sh</td></tr>
+<tr><th>STATUS:</th><td>404</td></tr>
+<tr><th>MESSAGE:</th><td>no such item</td></tr>
+<tr><th>SERVLET:</th><td>UserServlet</td></tr>
+</table>
+
+</body>
+</html>
